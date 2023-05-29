27378ec381e6ca02262419c3cbcd1552d0589089
Abdullah-Anaz <73793710+Abdullah-Anaz@users.noreply.github.com>
2023-05-29 20:39:27 +0530

    Update requests.js

--- src/requests.js
+++ src/requests.js
@@ -7,7 +7,6 @@ const requests = {
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 };
 
 export default requests;
